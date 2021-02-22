101596c6e83e71849430cb5ae5295cec6a29c788
William <wtbell4080@gmail.com>
2021-02-22 12:15:09 -0500

    Cleared the input value on reset click on line 46

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const savingsRate = .2;
 let necessitiesOutput = document.getElementById('necessities-output');
 let wantsOutput = document.getElementById('wants-output');
 let savingsOutput = document.getElementById('savings-output');
+let userInput = document.getElementById('income-input');
 
 //Event listener on the calculator form for submit
 document.getElementById('calculator-form').addEventListener('submit', calculateInput);
@@ -14,13 +15,14 @@ document.getElementById('clear-button').addEventListener('click', clearCalculato
 
 //get user input, perform calculations, and display results
 function calculateInput(e){
+  
   //variable to hold user input
-  let userInput = document.getElementById('income-input').value;
+  let userInputValue = userInput.value;
   
   //multiply input by rates
-  let necessitiesCalculation = userInput * necessitiesRate;
-  let wantsCalculation = userInput * wantsRate;
-  let savingsCalculation = userInput * savingsRate;
+  let necessitiesCalculation = userInputValue * necessitiesRate;
+  let wantsCalculation = userInputValue * wantsRate;
+  let savingsCalculation = userInputValue * savingsRate;
 
   //place commas in thousands mark
   let necessitiesAmount  = necessitiesCalculation.toLocaleString('en-US');
@@ -36,10 +38,12 @@ function calculateInput(e){
 }
 
 
+// Clear figures in all fields when reset button is clicked 
 function clearCalculator(e){
   necessitiesOutput.textContent = '$0';
   wantsOutput.textContent = '$0';
   savingsOutput.textContent = '$0';
+  userInput.value = "";
 
   e.preventDefault();
 }
